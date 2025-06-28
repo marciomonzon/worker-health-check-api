f013412f2b7a2c91d733623d8e4c0a1839d06470
monzondeveloper <monzondeveloper@gmail.com>
2025-06-27 22:06:15 -0300

    add logs to execute asyn method

---
+++ ApiHealthCheck/HealthCheckWorker/ApiHealthCheckerWorker.cs
@@ -0,0 +1,35 @@
+namespace HealthCheckWorker
+{
+    public class ApiHealthCheckerWorker(ILogger<ApiHealthCheckerWorker> _logger,
+                                       IConfiguration _configuration) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using var httpClient = new HttpClient();
+            var apiEndpoint = _configuration["ApiSettings:ApiHealthEndpoint"];
+            var secondsDelay = _configuration["WorkerSettings:SecondsDelay"];
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var response = await httpClient.GetAsync(apiEndpoint, stoppingToken);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        _logger.LogInformation("The API is Healthy. ({Time})", DateTimeOffset.Now);
+                    }
+                    else
+                    {
+                        _logger.LogWarning("The API is unhealthy. ({StatusCode}) ({Time})", response.StatusCode, DateTimeOffset.Now);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error on checking the Healthy of the API ({Time})", DateTimeOffset.Now);
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(int.Parse(secondsDelay!)), stoppingToken);
+            }
+        }
+    }
+}
--- ApiHealthCheck/HealthCheckWorker/Program.cs
+++ ApiHealthCheck/HealthCheckWorker/Program.cs
@@ -1,7 +1,7 @@
 using HealthCheckWorker;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddHostedService<ApiHealthCheckerWorker>();
 
 var host = builder.Build();
 host.Run();
